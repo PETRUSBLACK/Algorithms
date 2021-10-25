5a2b12d0449c0a0230088a4414a43b771d6d7c93
PETRUSBLACK <45966588+PETRUSBLACK@users.noreply.github.com>
2021-10-25 02:41:32 +0100

    adding to the linear searching algorith

--- linear_search.py
+++ linear_search.py
@@ -4,6 +4,16 @@ def linear_search(list, target):
             return i
     return None
 
+def verify(index):
+    if index is not None:
+        print("Target found @ index:", index)
+    else:
+        print("Target not found in list")
+
+
 
 number = [1,2,3,4,5,6,7,8,9,10]
-linear_search(number, 9)
+# linear_search(number, 9)
+
+result = linear_search(number, 12)
+verify(result)
